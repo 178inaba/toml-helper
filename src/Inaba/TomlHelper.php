ded940c24c53362b7ea5fc777e459f37865e783c
178inaba <178inaba@users.noreply.github.com>
2016-07-21 18:54:51 +0900

    to class

---
+++ src/Inaba/TomlHelper.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Inaba;
+
+class TomlHelper
+{
+    private static $instance;
+
+    public static function getInstance()
+    {
+        if (self::$instance === null) {
+        }
+    }
+}
